adbd643cd216218cc6ac5006b3a90fe6183f38d7
Sebenzile Mbambo <sebenzile2010@gmail.com>
2023-09-15 20:43:04 +0000

    fixed script.js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // variables used to add interactivity to current question,score counter and array of questions//
 const resetBtn = document.getElementById("reset");
 const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("reset");
-const submitBtn = document.getElementById("reset");
-const trueBtn = document.getElementById("reset");
-const falseBtn = document.getElementById("reset");
-const userScore = document.getElementById("user"); 
+const nextBtn = document.getElementById("next");
+const submitBtn = document.getElementById("submit");
+const trueBtn = document.getElementById("True");
+const falseBtn = document.getElementById("False");
+const userScore = document.getElementById("user-score"); 
 const questionText = document.getElementById("question-text");
 
 
