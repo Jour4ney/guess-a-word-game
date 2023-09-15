336c7e0b2803ed8c114fef873453c2580600d074
Sebenzile Mbambo <sebenzile2010@gmail.com>
2023-09-15 19:57:14 +0000

    added question, score and question variables to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// variables used to add interactivity to current question,score counter and array of questions//
+const resetBtn = document.getElementById("reset");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("reset");
+const submitBtn = document.getElementById("reset");
+const trueBtn = document.getElementById("reset");
+const falseBtn = document.getElementById("reset");
+const userScore = document.getElementById("user"); 
+const questionText = document.getElementById("question-text");
+
+
