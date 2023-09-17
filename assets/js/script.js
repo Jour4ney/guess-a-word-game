6a1b6720e1176d7f747fad98b63d737414565461
Sebenzile Mbambo <sebenzile2010@gmail.com>
2023-09-17 11:34:10 +0000

    added question structure to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// variables used to add interactivity to current question,score counter and array of questions//
+// variables used to represent and access elements in our document//
 const resetBtn = document.getElementById("reset");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
@@ -7,5 +7,185 @@ const trueBtn = document.getElementById("True");
 const falseBtn = document.getElementById("False");
 const userScore = document.getElementById("user-score"); 
 const questionText = document.getElementById("question-text");
+// variables used to create interactivity to current questions,score counter and array of questions//
+let currentQuestion = 0;
+var score = 0;
 
-
+let questions = [  
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+    {
+        question:"Which disease is not caused by the improper disposal of sewage waste?",
+        answer: [
+            { option:"Cholera",answer:false},
+            { option:"Heart attack",answer:true},
+        ]
+    },
+]
+// code that calls respective functions when a button is clicked,(on click events)//
